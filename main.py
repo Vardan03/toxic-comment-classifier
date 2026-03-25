8f304ff694fc404db768d091873ca087e308c557
Lina <lina.baghunts67@gmail.com>
2026-03-25 18:39:40 +0400

    Improve preprocessing, update main pipeline, fix requirements

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
-from src.data.load_data import load_train_data, load_test_data, load_test_labels, load_preprocess_train_data, load_preprocess_test_data, load_preprocess_test_labels
-from src.data.preprocess import preprocess_and_save, preprocess_train, remove_unlabeled_rows
+from src.data.load_data import load_train_data, load_test_data, load_test_labels
+from src.data.preprocess import preprocess_train, preprocess_test
 from src.models.train import train_model
 from src.features.tfidf import fit_tfidf
 from src.models.predict import predict, predict_proba
 from src.utils.metrics import compute_metrics, print_metrics
 from src.utils.logger import save_results
 
+
 def main():
+# Loading all the data
     train = load_train_data()
-    preprocess_and_save()
+    test = load_test_data()
+    test_labels = load_test_labels()
+
+# Preprocessing it 
     train = preprocess_train(train)
-    
-    vectors, vectorizer = fit_tfidf(train['comment_text'])
-    train_model(vectors, train, vectorizer)
+    test, test_labels = preprocess_test(test, test_labels)
 
-    test = load_preprocess_test_data()
-    test_labels = load_preprocess_test_labels() 
-    test_labels, test = remove_unlabeled_rows(test_labels, test)
+# Vectorization, meaning converting text into numbers
+    X_train_text, vectorizer = fit_tfidf(train["comment_text"])
 
+# Training model
+    train_model(X_train_text, train, vectorizer)
+
+# Making predictions
     y_pred = predict(test["comment_text"])
     y_pred_proba = predict_proba(test["comment_text"])
 
-    metrics = compute_metrics(test_labels.drop(columns=['id']).values, y_pred.values, y_pred_proba)
+# Getting the actual labels
+    y_true = test_labels.drop(columns=["id"]).values
+
+# Computing metrics
+    metrics = compute_metrics(
+        y_true,
+        y_pred.values,
+        y_pred_proba
+    )
+
     print_metrics(metrics)
     save_results(metrics, model_name="trained_baseline_model")
+
 
 if __name__ == "__main__":
     main()
